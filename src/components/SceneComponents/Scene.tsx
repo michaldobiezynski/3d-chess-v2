772d26bdae288820b56587532355e623f3382767
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-07-16 22:33:45 +0100

    use Lights created by me instead if Env component from drei to see the board

--- src/components/SceneComponents/Scene.tsx
+++ src/components/SceneComponents/Scene.tsx
@@ -1,36 +1,46 @@
 import React, { useCallback } from 'react'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import Board from './Board/Board'
 import { Canvas } from '@react-three/fiber'
 import { useAppDispatch, useAppSelector } from '../../store/store'
 import { Square } from 'chess.js'
 import { moveFigure, setSelectCell } from '../../store/gameSlice'
 
+import Lights from 'Lights'
+
 const Scene = () => {
-
   const dispatch = useAppDispatch()
-  const { board, figures, selectedCell, availableMoves, sceneBackground } = useAppSelector(state => state.game)
+  const { board, figures, selectedCell, availableMoves } = useAppSelector(
+    state => state.game,
+  )
 
-  const onCellSelect = useCallback((cell: Square) => {
-    dispatch(setSelectCell(cell))
-  }, [dispatch])
+  const onCellSelect = useCallback(
+    (cell: Square) => {
+      dispatch(setSelectCell(cell))
+    },
+    [dispatch],
+  )
 
-  const onFigureMove = useCallback((target: Square) => {
-    dispatch(moveFigure({ target }))
-  }, [dispatch])
+  const onFigureMove = useCallback(
+    (target: Square) => {
+      dispatch(moveFigure({ target }))
+    },
+    [dispatch],
+  )
 
   return (
     <main style={{ width: '100%', height: '100%' }}>
       <Canvas camera={{ fov: 90, position: [4, 5, -0.5] }}>
         <OrbitControls enablePan={false} minDistance={4} maxDistance={10} />
-        <Environment preset={sceneBackground} background />
+        <Lights />
         <Board
           board={board}
           figures={figures}
           selectedCell={selectedCell}
           onCellSelect={onCellSelect}
           availableMoves={availableMoves}
-          onFigureMove={onFigureMove} />
+          onFigureMove={onFigureMove}
+        />
       </Canvas>
     </main>
   )
